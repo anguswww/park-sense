a98b60d5635032a8ff25932c3b34f448fb1c2b5c
BDragonSoldier <ricky.oberholzer@gmail.com>
2025-10-25 12:30:20 +1100

    Add secrets.example.h

---
+++ firmware/src/secrets.example.h
@@ -0,0 +1,7 @@
+#pragma once
+// This file must be edited with your own credentials in order for the project to work
+// Secrets are kept in this (gitignored) file to avoid accidental leaks
+const char* WIFI_SSID = "SSID";
+const char* WIFI_PASS = "Password";
+const char* MQTT_USER = "user";
+const char* MQTT_PASS = "pass";
